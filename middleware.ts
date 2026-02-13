26986f711535f57dba4344499f136114899096f4
DomenGoli <domen.goli@gmail.com>
2026-02-13 19:34:05 +0100

    fix deploy

--- middleware.ts
+++ middleware.ts
@@ -1,25 +1,52 @@
 // import {authConfig} from "./authConfig";
 import NextAuth from "next-auth";
+import {
+    apiAuthPrefix,
+    // publicRoutes,
+    // authRoutes,
+} from "@/routes";
 import { authConfig } from "./app/_lib/auth.config";
-import { NextRequest } from "next/server";
 
 const { auth } = NextAuth(authConfig);
 
-async function authMiddleware(req: NextRequest) {
-    const {nextUrl} = req
-    const isApiAuthPrefix = nextUrl.pathname.startsWith("/auth/login");
-    const session = await auth()
+// export default auth(authCallback)
+export default auth((req) => {
+    const { nextUrl } = req;
+    const isLoggedIn = !!req.auth;
+    // const isLoggedIn = false;
+    console.log("ROUTE:", req.nextUrl.pathname);
+    console.log("IS LOGGED IN:", isLoggedIn);
 
-    if(isApiAuthPrefix) return null;
+    const isApiAuthPrefix = nextUrl.pathname.startsWith(apiAuthPrefix);
+    // const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+    // const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
-    if(nextUrl.pathname === "/" && !session) {
+    // if(nextUrl.pathname === "/") {
+    //     return Response.redirect(new URL("/auth/login", nextUrl));
+    // }
+
+    if (isApiAuthPrefix) {
+        return null;
+    }
+
+    if (!isLoggedIn) {
         return Response.redirect(new URL("/auth/login", nextUrl));
     }
-}
 
-export function middleware(request: NextRequest) {
-    return authMiddleware(request)
-}
+    // if (isAuthRoute) {
+    //     if (isLoggedIn)
+    //         return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    //     return null;
+    // }
+
+    return null;
+});
+
+
+
+
+
+
 
 export const config = {
     matcher: [
@@ -27,3 +54,4 @@ export const config = {
         // "/"
     ], // karkoli je v tej routi, bo invokal middleware (auth fukcijo v tem primeru)
 };
+// export const { auth: middleware } = NextAuth(authConfig);
---
+++ middlewareworking.ts
@@ -0,0 +1,29 @@
+// import {authConfig} from "./authConfig";
+import NextAuth from "next-auth";
+import { authConfig } from "./app/_lib/auth.config";
+import { NextRequest } from "next/server";
+
+const { auth } = NextAuth(authConfig);
+
+export default async function authMiddleware(req: NextRequest) {
+    const {nextUrl} = req
+    const isApiAuthPrefix = nextUrl.pathname.startsWith("/auth/login");
+    const session = await auth()
+
+    if(isApiAuthPrefix) return null;
+
+    if(nextUrl.pathname === "/" && !session) {
+        return Response.redirect(new URL("/auth/login", nextUrl));
+    }
+}
+
+// export function middleware(request: NextRequest) {
+//     return authMiddleware(request)
+// }
+
+export const config = {
+    matcher: [
+        "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+        // "/"
+    ], // karkoli je v tej routi, bo invokal middleware (auth fukcijo v tem primeru)
+};
