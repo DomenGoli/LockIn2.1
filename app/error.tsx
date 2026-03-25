13b2bc047ee416a1781046e2ffefd3c3d349bb4e
DomenGoli <domen.goli@gmail.com>
2026-03-25 11:29:50 +0100

    database

--- app/error.tsx
+++ app/error.tsx
@@ -15,7 +15,7 @@ export default function Error({ error, reset }: { error: ErrorObjectType, reset:
                 onClick={reset}
                 className="inline-block bg-accent-500 text-primary-800 px-6 py-3 text-lg"
             >
-                Try again
+                Try again!
             </button>
         </main>
     );
